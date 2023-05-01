67186b870116af1155457302540141aa8a2d32d1
swamptoday <maryna.nyzova.sa.2021@lpnu.ua>
2023-05-02 01:34:59 +0300

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Developer_form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Developer_form.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string fname, string lname, string email)
+        {
+            var model = new Welcome() { first_name = fname, last_name = lname, w_email = email };
+            return View(model);
+        }
+    }
+}
---
+++ Models/Welcome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Developer_form.Models
+{
+    public class Welcome
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string w_email { get; set; }
+    }
+}
